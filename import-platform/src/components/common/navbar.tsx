1881ddf9f78dece36dbfd44352ce2572a4d0b25a
prodaaron <aaronmesfin60@gmail.com>
2026-06-09 00:34:28 +0800

    nav-links basic completion

--- import-platform/src/components/common/navbar.tsx
+++ import-platform/src/components/common/navbar.tsx
@@ -12,8 +12,19 @@ export default function Navbar() {
         <div className="nav-links">
           <Link href="/">Home</Link>
           <Link href="/products">Products</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contacts">Contacts</Link>
+        </div>
+
+        <div className="nav-actions">
+          <Link href="/cart">Cart</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/profile">Profile</Link>
+        </div>
+
+        <div className="nav-auth">
+          <Link href="/auth/login">Login</Link>
+          <Link href="/auth/register">Register</Link>
         </div>
       </div>
     </nav>
